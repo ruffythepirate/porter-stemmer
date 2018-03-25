b68682aadd6941d95b09a1ac47a4fe779d116c90
ruffy <johan.ellfries@gmail.com>
2018-03-25 11:43:15 +0200

    Implement stage two of porter stemmer.

--- src/main/scala/PorterStemmer.scala
+++ src/main/scala/PorterStemmer.scala
@@ -35,6 +35,37 @@ object PorterStemmer {
     }
   }
 
+  def stageTwo(word: String) = {
+    val measure = MeasureHelper.determineMeasure(word)
+    word match {
+      case s if endsWithAndHasMeasureBiggerThanZero(word, "ational") => s.dropRight(7) + "ate"
+      case s if endsWithAndHasMeasureBiggerThanZero(word, "tional") => s.dropRight(6) + "tion"
+      case s if endsWithAndHasMeasureBiggerThanZero(word, "enci") => s.dropRight(4) + "ence"
+      case s if endsWithAndHasMeasureBiggerThanZero(word, "anci") => s.dropRight(4) + "ance"
+      case s if endsWithAndHasMeasureBiggerThanZero(word, "izer") => s.dropRight(4) + "ize"
+      case s if endsWithAndHasMeasureBiggerThanZero(word, "abli") => s.dropRight(4) + "able"
+      case s if endsWithAndHasMeasureBiggerThanZero(word, "alli") => s.dropRight(4) + "al"
+      case s if endsWithAndHasMeasureBiggerThanZero(word, "entli") => s.dropRight(5) + "ent"
+      case s if endsWithAndHasMeasureBiggerThanZero(word, "eli") => s.dropRight(3) + "e"
+      case s if endsWithAndHasMeasureBiggerThanZero(word, "ousli") => s.dropRight(5) + "ous"
+      case s if endsWithAndHasMeasureBiggerThanZero(word, "ization") => s.dropRight(7) + "ize"
+      case s if endsWithAndHasMeasureBiggerThanZero(word, "ation") => s.dropRight(5) + "ate"
+      case s if endsWithAndHasMeasureBiggerThanZero(word, "ator") => s.dropRight(4) + "ate"
+      case s if endsWithAndHasMeasureBiggerThanZero(word, "alism") => s.dropRight(5) + "al"
+      case s if endsWithAndHasMeasureBiggerThanZero(word, "iveness") => s.dropRight(7) + "ive"
+      case s if endsWithAndHasMeasureBiggerThanZero(word, "fulness") => s.dropRight(7) + "ful"
+      case s if endsWithAndHasMeasureBiggerThanZero(word, "ousness") => s.dropRight(7) + "ous"
+      case s if endsWithAndHasMeasureBiggerThanZero(word, "aliti") => s.dropRight(5) + "al"
+      case s if endsWithAndHasMeasureBiggerThanZero(word, "iviti") => s.dropRight(5) + "ive"
+      case s if endsWithAndHasMeasureBiggerThanZero(word, "biliti") => s.dropRight(6) + "ble"
+      case s => s
+    }
+  }
+
+  private def endsWithAndHasMeasureBiggerThanZero(word:String, ending: String) = {
+    word.endsWith(ending) && MeasureHelper.determineMeasure(word.dropRight(ending.length)) > 0
+  }
+
   val NON_APPLIED_DOUBLE_CONSONANTS = Seq('l', 's', 'z')
 
   private def stageOneBOnSecondOrThirdTrigger(word: String) = {
---
+++ src/test/scala/StageTwoTest.scala
@@ -0,0 +1,38 @@
+package io.ruffy.porterstemmer
+
+import org.scalatest.{FlatSpec, MustMatchers}
+
+class StageTwoTest extends FlatSpec with MustMatchers {
+
+
+  val stageTwoTestCases = Seq(
+
+    ("relational", "relate"),
+    ("conditional", "condition"),
+    ("rational", "rational"),
+    ("valenci", "valence"),
+    ("hesitanci", "hesitance"),
+    ("digitizer", "digitize"),
+    ("conformabli", "conformable"),
+    ("radicalli", "radical"),
+    ("differentli", "different"),
+    ("vileli", "vile"),
+    ("analogousli", "analogous"),
+    ("vietnamization", "vietnamize"),
+    ("predication", "predicate"),
+    ("operator", "operate"),
+    ("feudalism", "feudal"),
+    ("decisiveness", "decisive"),
+    ("hopefulness", "hopeful"),
+    ("callousness", "callous"),
+    ("formaliti", "formal"),
+    ("sensitiviti", "sensitive"),
+    ("sensibiliti", "sensible")
+  )
+
+  stageTwoTestCases.foreach(test => {
+    it should s"transform ${test._1} -> ${test._2}" in {
+      PorterStemmer.stageTwo(test._1) must equal(test._2)
+    }
+  })
+}
